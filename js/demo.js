648c753076e2c094228ccbdd2bf33356598a1de1
TeemuHe <teemuhoo@luukku.com>
2019-10-02 08:28:02 +0300

    calculator created

--- js/demo.js
+++ js/demo.js
@@ -4,10 +4,10 @@ function writeLines() {
     console.log("Pitäisi olla a");
     console.log("Kirjattiin", document.getElementById("myLetter").value)
     var letter = document.getElementById("myLetter").value;
-    if(letter.toLowerCase() == 'a'){
+    if (letter.toLowerCase() == 'a') {
         console.log("OIKEIN");
     }
-    else{
+    else {
         console.log("VÄÄRIN");
     }
 }
@@ -15,11 +15,11 @@ function numberChecker() {
     console.log("Pitäisi olla 1-10");
     console.log("Kirjattiin", document.getElementById("myNumberValue").value);
     var number = document.getElementById("myNumberValue").value;
-    if(number >= 1 && number <= 10){
+    if (number >= 1 && number <= 10) {
         console.log("OIKEIN");
         //console.log(number * number);
     }
-    else{
+    else {
         console.log("VÄÄRIN");
     }
 }
@@ -28,5 +28,36 @@ function readLocation() {
     var address = document.getElementById("address").value;
     console.log(document.getElementById("city").value);
     var city = document.getElementById("city").value;
-    document.getElementById('mapFrame').src= 'https://www.google.com/maps?q=' + address + city + '&output=embed';
+    document.getElementById('mapFrame').src = 'https://www.google.com/maps?q=' + address + city + '&output=embed';
+}
+function decreaseNr1() {
+    var firstNumber = document.getElementById("firstNumber").value;
+    var newValue = firstNumber - 1;
+    document.getElementById("firstNumber").value = newValue;
+    console.log(newValue);
+}
+function increaseNr1() {
+    var firstNumber = document.getElementById("firstNumber").value;
+    var newValue = Number(firstNumber) + 1;
+    document.getElementById("firstNumber").value = newValue;
+    console.log(newValue);
+}
+function decreaseNr2() {
+    var secondNumber = document.getElementById("secondNumber").value;
+    var newValue = secondNumber - 1;
+    document.getElementById("secondNumber").value = newValue;
+    console.log(newValue);
+}
+function increaseNr2() {
+    var secondNumber = document.getElementById("secondNumber").value;
+    var newValue = Number(secondNumber) + 1;
+    document.getElementById("secondNumber").value = newValue;
+    console.log(newValue);
+}
+window.onload = function () {
+    this.getRndInteger();
+}
+function getRndInteger() {
+    document.getElementById('firstNumber').value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    document.getElementById('secondNumber').value = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 }
